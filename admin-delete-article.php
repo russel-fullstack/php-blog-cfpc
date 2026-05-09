7f43111d71a811695b131c8691b909d194f789fa
russel-fullstack <russeldev95@gmail.com>
2026-05-09 11:57:03 +0100

    feat(delete)

---
+++ admin-delete-article.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+require_once 'database/database.php';
+require_once 'flash.php';
+require_once 'app/enums/role.php';
+require_once 'app/helpers.php';
+
+
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== Role::ADMIN->value) {
+    header('Location: index.php');
+    exit();
+}
+
+$query = $pdo->prepare('DELETE FROM articles WHERE id = :id');
+$query->execute([':id' => $_GET['id']]);
+
+header('Location: admin-list-article.php');
+exit(); 
