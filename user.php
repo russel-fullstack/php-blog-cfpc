8294d51c7ecb68899f19ab95ffccd89697bc7f16
russel-fullstack <russeldev95@gmail.com>
2026-05-12 14:48:25 +0100

    refactor

--- user.php
+++ user.php
@@ -6,7 +6,7 @@ require_once 'database/database.php';
 
 $pageTitle = "page user";
 ob_start();
-require_once 'resources/views/users/user_html.Php';
+require_once 'resources/views/users/user_html.php';
 
 $pageContent = ob_get_clean(); // Récupérer le contenu du tampon de sortie et le stocker dans la variable $pageContent
 require_once 'resources/views/layouts/user-layout/user-layout_html.php'; //Inclure le layout du blog qui affichera le header, le contenu et le footer
